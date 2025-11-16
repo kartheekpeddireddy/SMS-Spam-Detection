faf5b4e9a3c55bc9bff83a0427298e8283a834f9
kartheekpeddireddy <kartheekpeddireddy975@gmail.com>
2025-11-16 19:58:36 +0530

    Delete src directory

--- src/feature_extraction.py
+++
@@ -1,6 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-def get_tfidf_features(texts, max_features=3000):
-    vectorizer = TfidfVectorizer(max_features=max_features, stop_words='english')
-    X = vectorizer.fit_transform(texts)
-    return X, vectorizer
